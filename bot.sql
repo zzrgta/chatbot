f0df676bda8e8222035edee13eb63525e6d15e4d
zzrgta <52562770+zzrgta@users.noreply.github.com>
2023-04-09 14:54:50 +0800

    Add files via upload

---
+++ bot.sql
@@ -0,0 +1,21 @@
+insert into videos values('Hong Kong','https://www.bilibili.com/video/BV1G34y1p7Xn/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Taipei','https://www.bilibili.com/video/BV1bx411d7pZ/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Macao','https://www.bilibili.com/video/BV1bx411d7sK/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Nanchang','https://www.bilibili.com/video/BV1bx411d7qh/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Shanghai','https://www.bilibili.com/video/BV1sx411d7GU/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Shenyang','https://www.bilibili.com/video/BV1Mx411Z7BX/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Changsha','https://www.bilibili.com/video/BV1Mx411Z76D/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Suzhou','https://www.bilibili.com/video/BV15x411Z7xq/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Guangzhou','https://www.bilibili.com/video/BV15x411Z766/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Chengdu','https://www.bilibili.com/video/BV15x411Z71G/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Hangzhou','https://www.bilibili.com/video/BV15x411Z7Dr/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Xi`an','https://www.bilibili.com/video/BV15x411Z7zD/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Tianjin','https://www.bilibili.com/video/BV184411G7aK/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Shenzhen','https://www.bilibili.com/video/BV1P4411G7Ne/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Qingdao','https://www.bilibili.com/video/BV1c4411G7oH/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Chongqing','https://www.bilibili.com/video/BV1P4411575q/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Dali','https://www.bilibili.com/video/BV1FP4y1P7Gt/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Xizang','https://www.bilibili.com/video/BV1h64y1o7RA/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+insert into videos values('Wuhan','https://www.bilibili.com/video/BV1fy4y1z7MH/?share_source=copy_web&vd_source=ac6832cbd2dad6d752b9339b7ffd4db8');
+
+
